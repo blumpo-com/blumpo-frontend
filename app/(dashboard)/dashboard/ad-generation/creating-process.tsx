8af426e69b229d8d790ddbf4f2804babc5895e3c
Jakub Madry <jaiwiecejmnie@gmail.com>
2026-02-08 19:22:43 +0100

    Change play the game button style

--- app/(dashboard)/dashboard/ad-generation/creating-process.tsx
+++ app/(dashboard)/dashboard/ad-generation/creating-process.tsx
@@ -107,10 +107,7 @@ const LeftPanel = memo(function LeftPanel({ onPlay }: LeftPanelProps) {
 
         {/* CTA Button */}
         <button className={styles.playButton} onClick={onPlay} type="button">
-          <span>Play with Blumpo</span>
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6 4L10 8L6 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
+          Play the game
         </button>
       </div>
     </div>
